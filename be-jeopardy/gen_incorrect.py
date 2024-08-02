75df3348aed3a64a3f915e397a1d95821e36b383
Riley Thompson <rileythompson99@gmail.com>
2024-08-01 22:14:51 -0400

    add example of gpt incorrect answers

--- be-jeopardy/gen_incorrect.py
+++ be-jeopardy/gen_incorrect.py
@@ -17,13 +17,13 @@ You will be given a question in the form: `question: "question_text", answer: "c
 and you need to respond with 3 incorrect but plausible answers to the question in the form `['incorrect_1', 'incorrect_2', 'incorrect_3'].
 '''
 
-header = False
 with open('input.csv', 'r') as input_file, open('output.csv', 'w') as output_file:
     input_reader = csv.reader(input_file)
     output_writer = csv.writer(output_file)
     headers = ['id', 'question', 'answer', 'alternatives',
                'incorrect', 'source', 'tags', 'other']
     output_writer.writerow(headers)
+    header = False
     for row in input_reader:
         if not header:
             header = True
@@ -41,3 +41,7 @@ with open('input.csv', 'r') as input_file, open('output.csv', 'w') as output_fil
         )
         row[4] = chat_completion.choices[0].message.content
         output_writer.writerow(row)
+
+# question: Who painted the ceiling of the Sistine chapel?
+# answer:   Michelangelo
+# incorrect: { Raphael, Leonardo da Vinci, Donatello }
